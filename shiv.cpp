b3d0a1dd00d4715fb2155b0321c8e092d66769e2
shivendra verma <shivendra.verma@navyuginfo.com>
2018-04-02 12:09:50 +0530

    add fuction is also added

--- shiv.cpp
+++ shiv.cpp
@@ -1,10 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int fact(n){
+int fact(int n){
 	if(n==1 or n==0)
 		return 1;
 	else
 		return n*fact(n-1);
+}
+int sum(int a , int b){
+	return (a+b);
 }
 int main(){
 	int n;
